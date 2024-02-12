8b2ab3c6d8c59505e022cdfa6064c63dc4a5e3a6
Md. Maharul Alam Rasel <maharulalam@gmail.com>
2024-02-12 19:14:53 +0600

    new project

---
+++ app.js
@@ -0,0 +1,54 @@
+let items = document.querySelectorAll('.slider .List .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
+
+let countItem = items.length;
+let itemActive = 0;
+
+next.onclick = function(){
+    itemActive = itemActive +1 ;
+    if(itemActive >= countItem){
+        itemActive = 0;
+    }
+    showSlider();
+}
+
+prev.onclick = function(){
+    itemActive = itemActive -1;
+    if(itemActive <0){
+        itemActive = countItem -1
+    }
+    showSlider();
+}
+
+// -----auto run slider---
+
+let refreshInterval = setInterval(()=>{
+    next.click();
+},3000)
+
+
+function showSlider(){
+    let itemActiveOld = document.querySelector('.slider .List .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
+
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
+
+// --------clear auto time run slider----
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=>{
+        next.click();
+    },3000)
+
+}
+
+thumbnails.forEach((thumbnail, index)=>{
+    thumbnail.addEventListener('click', ()=>{
+        itemActive = index;
+        showSlider();
+    })
+})
